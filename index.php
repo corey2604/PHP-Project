56450de247e1ccdfe85ea6550e8a36956542f300
Corey Duffy <c.duffy@kainos.com>
2019-08-05 12:07:42 +0100

    Adding required libraries for API requests

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Initial File";
- ?>
+require 'vendor/autoload.php';
+
+$API_KEY = "1fd53134883fb4e950c4500c87738789";
+$client = new \GuzzleHttp\Client();
+$response = $client->get("https://api.themoviedb.org/3/movie/550", [
+  'query' => [
+    'api_key' => $API_KEY,
+  ]
+]);
+$value = json_decode($response->getBody(), true);
+$adult = $value['budget'];
+echo "BUDGET : $adult";
+?>
