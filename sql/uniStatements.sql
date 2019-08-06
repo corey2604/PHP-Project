184dbe3b281e2efe88cf9ed7aeae76dc6aa3883e
Corey Duffy <c.duffy@kainos.com>
2019-08-06 11:11:15 +0100

    Adding SQL statements for new tables

--- sql/uniStatements.sql
+++ sql/uniStatements.sql
@@ -7,3 +7,36 @@ CREATE TABLE `ProjectUsers` (
  `reg_date` datetime NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ProjectMovies` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `title` varchar(50) NOT NULL,
+ `overview` varchar(500) NOT NULL,
+ `poster_path` varchar(50),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ProjectGenres` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `api_id` int(11) NOT NULL,
+ `name` varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ProjectUserMovies` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user_id` int(11) NOT NULL,
+ `movie_id` int(11) NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (user_id) REFERENCES ProjectUsers(id),
+  FOREIGN KEY (blocked_user_id) REFERENCES ProjectMovies(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ProjectMovieGenres` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `movie_id` int(11) NOT NULL,
+ `genre_id` int(11) NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (movie_id) REFERENCES ProjectMovies(id),
+  FOREIGN KEY (genre_id) REFERENCES ProjectGenres(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
