ac7062251af3a8504b47c20013db9621ab2bb710
Corey Duffy <c.duffy@kainos.com>
2019-08-06 10:53:53 +0100

    Adding logout functionality and code cleanup for navbar

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -1,23 +1,27 @@
 <?php
-function getNavbar() {
-  echo "<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
-  <a class=\"navbar-brand\" href=\"#\">Navbar</a>
-  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
-    <span class=\"navbar-toggler-icon\"></span>
+function getNavbar() { ?>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
   </button>
-  <div class=\"collapse navbar-collapse\" id=\"navbarNav\">
-    <ul class=\"navbar-nav\">
-      <li class=\"nav-item active\">
-        <a class=\"nav-link\" href=\"index.php\">Search for movie <span class=\"sr-only\">(current)</span></a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Search for movie <span class="sr-only">(current)</span></a>
       </li>
-      <li class=\"nav-item\">
-        <a class=\"nav-link\" href=\"currentlyShowing.php\">Currently showing movies</a>
+      <li class="nav-item">
+        <a class="nav-link" href="currentlyShowing.php">Currently showing movies</a>
       </li>
-      <li class=\"nav-item\">
-        <a class=\"nav-link\" href=\"myMovies.php\">My movies</a>
+      <li class="nav-item">
+        <a class="nav-link" href="myMovies.php">My movies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Log Out</a>
       </li>
     </ul>
   </div>
-</nav>";
+</nav>
+<?php
 }
 ?>
